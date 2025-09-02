c0161ae706e3070297009c235cb34d5721a538a2
SharathHebbar <sharathhebbar24@gmail.com>
2025-09-02 16:25:43 +0530

    Agentic Chatbot using LangGraph

---
+++ Gen AI Apps/AgenticChatbot/src/langgraph_agenticai/nodes/basic_chatbot_node.py
@@ -0,0 +1,17 @@
+from src.langgraph_agenticai.state.state import State
+
+class BasicChatbotNode:
+    """
+    Basic Chatbot login implementation
+    """
+
+    def __init__(self, model):
+        self.llm = model
+    
+    def process(self, state: State) -> dict:
+        """
+        Processess the input state and generates a chatbot response.
+        """
+
+        return {"messages": self.llm.invoke(state['messages'])}
+        
